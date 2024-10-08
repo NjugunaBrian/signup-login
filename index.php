7c7a564c40106b24e6dda3a3b19f0f4f4413fb93
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-10-08 21:25:52 +0300

    database connection

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+<body>
+
+<h3>Login</h3>
+
+<form action="includes/login.inc.php" method="POST">
+    <input type="text" name="username" placeholder="Username"  />
+    <input type="text" name="pwd" placeholder="Password"  />
+    <button>Sign Up</button>
+</form>
+
+<h3>Sign Up</h3>
+
+<form action="includes/signup.inc.php" method="POST">
+    <input type="text" name="username" placeholder="Username"  />
+    <input type="text" name="pwd" placeholder="Password"  />
+    <input type="text" name="email" placeholder="Email"  />
+    <button>Sign Up</button>
+</form>
+    
+</body>
+</html>
