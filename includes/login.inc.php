1b3acfb6dd671956d2c2ab13b9b8e5c340d41e93
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-10-16 21:02:08 +0300

    feat: Login error handlers

---
+++ includes/login.inc.php
@@ -0,0 +1,54 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+
+    try{
+        require_once 'dbh.inc.php';
+        require_once 'login_model.inc.php';
+        require_once 'login_contr.inc.php';
+
+        //error handlers
+        $errors = [];
+        if(is_input_empty($username, $pwd)){
+            $errors["empty_input"] = "Fill in all the fields!";
+        }
+
+        $result = get_user($pdo, $username);
+
+        if(is_username_wrong($result)){
+            $errors["login_incorrect"] = "Incorrect login info!";
+        }
+
+        if(!is_username_wrong($result) && is_password_wrong($pwd, $result["pwd"])){
+            $errors["login_incorrect"]  = "Incorrect login info!";
+        }
+        
+
+        require_once 'config_session.inc.php';
+
+        if($errors){
+            $_SESSION["signup_errors"] = $errors;
+
+           
+
+            header("Location: ../index.php");
+            die();
+        }
+
+        $newSessionId = session_create_id();
+        $sessionId = $newSessionId . "_" . $result["id"];
+        $session_id($sessionId);
+
+
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+
+    }
+
+} else {
+    header("Location: ../index.php");
+    die();
+}
